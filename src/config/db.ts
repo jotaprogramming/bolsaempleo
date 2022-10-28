d5a7006c7a175b2805b97fc6607d8a89866400a2
Jerson Guerrero <yesidlds54@gmail.com>
2022-10-28 10:28:55 -0500

    ORM configuration

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+import { IORM } from '../utils/interfaces/db.interface';
+import { PrismaClient } from '@prisma/client'
+
+class DBPrisma implements IORM<PrismaClient> {
+    private _orm: PrismaClient = new PrismaClient();
+
+    get orm(): PrismaClient {
+        return this._orm;
+    }
+}
+
+const prisma = new DBPrisma().orm
+
+export default prisma
