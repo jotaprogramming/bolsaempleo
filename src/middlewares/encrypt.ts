ef8cdf19a20b2138f9884cd055ac85a0e9d00fc4
Jerson Guerrero <yesidlds54@gmail.com>
2022-11-04 22:38:52 -0500

    Fixes encryption error during the Update action

--- src/middlewares/encrypt.ts
+++ src/middlewares/encrypt.ts
@@ -4,12 +4,11 @@ import { Prisma } from '@prisma/client';
 
 /* A middleware that is used to hash the password before it is saved in the database. */
 prisma.$use(async (params: Prisma.MiddlewareParams, next: any) => {
-	if (
-		(params.model === 'users' && params.action === 'create') ||
-		params.action === 'update'
-	) {
-		const password = params.args.data.password;
-		params.args.data.password = bcrypt.hashSync(password, 10);
+	if (params.model === 'users') {
+		if (params.action === 'create' || params.action === 'update') {
+			const password = params.args.data.password;
+			params.args.data.password = bcrypt.hashSync(password, 10);
+		}
 	}
 	return await next(params);
 });
